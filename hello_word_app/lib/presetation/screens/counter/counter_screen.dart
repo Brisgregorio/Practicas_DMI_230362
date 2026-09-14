8852b559e882ec19c141b71f3de3f34c4117b5f2
Brisgregorio <230362@utxicotepec.edu.mx>
2026-09-14 10:08:40 -0600

    Tarea Flutter
    Se realizo la primera tarea de la practica cambio de colores y tipo de letra

--- hello_word_app/lib/presetation/screens/counter/counter_screen.dart
+++ hello_word_app/lib/presetation/screens/counter/counter_screen.dart
@@ -1,64 +1,105 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CounterFunctionsScreen extends StatefulWidget {
   const CounterFunctionsScreen({super.key});
 
   @override
-  State<CounterFunctionsScreen> createState() => _CounterFunctionsScreenState();
+  State<CounterFunctionsScreen> createState() =>
+      _CounterFunctionsScreenState();
 }
 
 class _CounterFunctionsScreenState extends State<CounterFunctionsScreen> {
   int clickCounter = 0;
 
+  Color get counterColor {
+    if (clickCounter > 0) {
+      return Colors.green;
+    }
+
+    if (clickCounter < 0) {
+      return Colors.red;
+    }
+
+    return Colors.blue;
+  }
+
+  void increaseCounter() {
+    setState(() {
+      clickCounter++;
+    });
+  }
+
+  void decreaseCounter() {
+    setState(() {
+      clickCounter--;
+    });
+  }
+
+  void resetCounter() {
+    setState(() {
+      clickCounter = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Counter Functions'),
+        title: Text(
+          'Counter Functions',
+          style: GoogleFonts.nunito(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         actions: [
           IconButton(
-            icon: Icon(Icons.refresh_rounded),
-            onPressed: () {
-              setState(() {
-                clickCounter = 0;
-              });
-            }, 
+            icon: const Icon(Icons.refresh_rounded),
+            tooltip: 'Reiniciar contador',
+            onPressed: resetCounter,
           ),
         ],
       ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('$clickCounter',
-                style: const TextStyle( 
-                  fontSize: 160, fontWeight: FontWeight.w100 )),
-              Text('Click${clickCounter == 1 ? '' : 's' }', style: TextStyle( fontSize: 25 ))
-            ],
-          ),
-        ),
-        floatingActionButton: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            FloatingActionButton(
-              onPressed: () {
-                clickCounter ++;
-                setState(() {});
-              },
-              child: const Icon( Icons.plus_one ),
+            Text(
+              '$clickCounter',
+              style: GoogleFonts.nunito(
+                fontSize: 160,
+                fontWeight: FontWeight.w300,
+                color: counterColor,
+              ),
             ),
-            
-            SizedBox(height: 15,),
-
-            FloatingActionButton(
-              onPressed: () {
-                clickCounter --;
-                setState(() {});
-              },
-              child: const Icon( Icons.exposure_minus_1_outlined ),
+            Text(
+              'Click${clickCounter == 1 ? '' : 's'}',
+              style: GoogleFonts.nunito(
+                fontSize: 25,
+                fontWeight: FontWeight.w600,
+                color: counterColor,
+              ),
             ),
           ],
-        )
-      );
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: 'increase_button',
+            onPressed: increaseCounter,
+            child: const Icon(Icons.plus_one),
+          ),
+          const SizedBox(height: 15),
+          FloatingActionButton(
+            heroTag: 'decrease_button',
+            onPressed: decreaseCounter,
+            child: const Icon(Icons.exposure_minus_1_outlined),
+          ),
+        ],
+      ),
+    );
   }
 }
